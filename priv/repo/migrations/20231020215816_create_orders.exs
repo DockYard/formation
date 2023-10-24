c29818929300a619d5d441bc22f46955aa9de7c5
Bennie Rosas <ben@aliencyb.org>
2023-10-24 13:32:48 -0500

    use jsonb for items column

--- priv/repo/migrations/20231020215816_create_orders.exs
+++ priv/repo/migrations/20231020215816_create_orders.exs
@@ -3,11 +3,11 @@ defmodule Formation.Repo.Migrations.CreateOrders do
 
   def change do
     create table(:orders) do
-      add :name, :string
       add :customer, :string
+      add :items, :jsonb, default: "[]"
+      add :name, :string
       add :price, :float
       add :status, :string
-      add :items, :string
 
       timestamps(type: :utc_datetime)
     end
